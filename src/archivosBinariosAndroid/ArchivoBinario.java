7acfdc12e9157882718b4b26ca52be47ee268427
WM <WM@DESKTOP-7PSHG7J>
2017-11-14 23:45:39 -0600

    Actualización clase ArchivoBinario.java

--- src/archivosBinariosAndroid/ArchivoBinario.java
+++ src/archivosBinariosAndroid/ArchivoBinario.java
@@ -1,17 +1,20 @@
-/**
- * Clase que modelará y manejará ficheros binarios. Crear, escribir, leer y
- * comparar son de las acciones que podrá realizar esta clase
- */
 package archivosBinariosAndroid;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+/**
+ * Clase que modelará y manejará ficheros binarios. Crear, escribir, leer y
+ * comparar son de las acciones que podrá realizar esta clase
+ *
+ * @author WM
+ */
 public class ArchivoBinario {
 
     private File archivo; //manejará fichero
@@ -30,7 +33,7 @@ public class ArchivoBinario {
      * objeto que apunta a la info necesaria de la SD, así como inicializa los
      * flujos
      */
-    public ArchivoBinario() {
+    public ArchivoBinario() throws FileNotFoundException {
 
         infoSD = new TarjetaSD();
         archivo = new File(infoSD.getRuta(), "archivo.txt"); //nombre por default del archivo
@@ -40,6 +43,8 @@ public class ArchivoBinario {
         entrada = null;
         fos = null;
         salida = null;
+        fis = new FileInputStream(archivo);
+        entrada = new DataInputStream(fis);
     }
 
     /**
@@ -55,10 +60,10 @@ public class ArchivoBinario {
         archivo = new File(infoSD.getRuta(), nombreArchivo); //nombre por default del archivo
         contenido_archivo = ""; //por default no tiene contenido
         //inicializamos flujos
-        fis = null;
-        entrada = null;
         fos = new FileOutputStream(archivo);
         salida = new DataOutputStream(fos);
+        fis = new FileInputStream(archivo);
+        entrada = new DataInputStream(fis);
     }
 
     /**
@@ -74,8 +79,8 @@ public class ArchivoBinario {
         this.archivo = new File(archivo.getPath(), archivo.getName()); //nombre por default del archivo
         contenido_archivo = ""; //por default no tiene contenido
         //inicializamos flujos
-        fis = null;
-        entrada = null;
+        fis = new FileInputStream(archivo);
+        entrada = new DataInputStream(fis);
         fos = new FileOutputStream(archivo);
         salida = new DataOutputStream(fos);
     }
@@ -91,9 +96,16 @@ public class ArchivoBinario {
     public boolean llenaAleatorio(int cantidad_de_aleatorios) {
 
         boolean seLlenoCorrectamente = false; //informará si el fichero binario se llenó correctamente con un 'true', si devuelve 'false' es que hubo alguna excepción
+
+        fos = null;
+        salida = null;
+
         //si tenemos SD disponible y podemos escribir sobre ella
         if (infoSD.TieneSD() && infoSD.PuedeEscribir()) {
             try {
+
+                fos = new FileOutputStream(archivo);
+                salida = new DataOutputStream(fos);
 
                 //Escribimos n números aleatorios
                 int n = 10;
@@ -129,6 +141,7 @@ public class ArchivoBinario {
         return seLlenoCorrectamente;
     }
 
+    //Agregar más formas de llenar el fichero(String, float, etc...)
     /**
      * Lee contenido de archivo binario. Devuelve un String con los elementos
      * 'formateados' listos para imprimir pero además almacena el contenido REAL
@@ -137,48 +150,82 @@ public class ArchivoBinario {
      *
      * @return contenido_fichero_formateado
      */
-    public String leerFichero() {
+    public String leerFichero() throws FileNotFoundException {
 
-        //boolean seLeyoCorrectamente = false; //informará si el fichero binario se leyó  correctamente con un 'true', si devuelve 'false' es que hubo alguna excepción
-        String contenido_archivo_formateado = "";
-        //PODEMOS TENER OTRO STRING PARA TENER EL CONTENIDO 'FORMATEADO' PARA IMPRIMIR Y LA VARIABLE DE ARRIBA UTILIZAR PARA LA COMPARACIÓN
-        //si tenemos SD disponible
-        if (infoSD.TieneSD()) {
+        fis = null;
+        entrada = null;
+
+        String contenido_formateado = "";
+        int n;
+        try {
+
+            fis = new FileInputStream(archivo);
+            entrada = new DataInputStream(fis);
+
+            //LEEMOS ARCHIVO
+            while (true) {
+                n = entrada.readInt();  //se lee  un entero del fichero
+                contenido_formateado = contenido_formateado + n + " - ";
+                System.out.println(n);  //se muestra en pantalla
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("Fin de fichero");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
             try {
-
-                int n = 0; //irá leyendo int por int el archivo
-
-                //LEEMOS ARCHIVO
-                while (true) {
-                    n = entrada.readInt();  //se lee siguiente Int en el fichero
-                    this.contenido_archivo += n; //vamos concatenando los elementosC
-                    contenido_archivo_formateado = contenido_archivo_formateado + n + " - "; //vamos concatenando los elementosC
-
-                    //System.out.println(n);  //se muestra en pantalla
+                if (fis != null) {
+                    fis.close();
                 }
-                //seLeyoCorrectamente = true; //sólo si se ejecuta correctamente, devolvemos true
-
-            } catch (FileNotFoundException e) {
-                System.out.println(e.getMessage());
+                if (entrada != null) {
+                    entrada.close();
+                }
             } catch (IOException e) {
                 System.out.println(e.getMessage());
-            } finally {
-                //Cerramos flujos 
-                try {
-                    if (fos != null) {
-                        fos.close();
-                    }
-                    if (salida != null) {
-                        salida.close();
-                    }
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
             }
         }
 
-        //return seLeyoCorrectamente;
-        return contenido_archivo_formateado;
+        return contenido_formateado;
     }
 
+    //Agregar más formas de leer el fichero(String, float, etc...)
+    //Métodos para comparación de ficheros
+    //getters y setters necesarios
+    public TarjetaSD getInfoSD() {
+        return infoSD;
+    }
+
+    public void setInfoSD(TarjetaSD infoSD) {
+        this.infoSD = infoSD;
+    }
+
+    public String getContenido_archivo() {
+        return contenido_archivo;
+    }
+
+    public void setContenido_archivo(String contenido_archivo) {
+        this.contenido_archivo = contenido_archivo;
+    }
 }//fin class ArchivoBinario
+
+/**
+ * DESPUÉS VEMOS ESTA ALTERNATIVA!!! Clase que hereda de archivoBinario y
+ * servirá para comparar 2 ficheros binarios. Hereda de clase ArchivoBinario por
+ * tanto reutiliza la mayoría de elemnetos de clase Padre y
+ *
+ * @author WM
+ */
+
+/*
+ class ComparaArchivosBinarios extends ArchivoBinario {
+
+    
+ public ComparaArchivosBinarios() {
+ super();
+ }
+
+    
+ }
+ */
--- src/vic/men/lan/android/MainActivity.java
+++ src/vic/men/lan/android/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -21,15 +22,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+import archivosBinariosAndroid.ArchivoBinario;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MainActivity extends Activity implements OnClickListener {
 
+    //Controles View
     Button leersd;
     Button escribirsd;
     TextView textView;
 
-    //con estos atributos controlamos el estado de la SD
-    boolean sdDisponible = false;
-    boolean sdAccesoEscritura = false;
+    //Objeto que dará acceso a la lógica para manejar Archivos binarios
+    ArchivoBinario manejaFicheroBinario;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,14 @@ public class MainActivity extends Activity implements OnClickListener {
         escribirsd = (Button) findViewById(R.id.escribirsd);
         textView = (TextView) findViewById(R.id.textView);
 
+        try {
+            //inicializamos objeto manejaFicheroBinario enviando nombre del archivo a crear o buscar
+            manejaFicheroBinario = new ArchivoBinario("binario1.txt");
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         //escucha a botonnes
         leersd.setOnClickListener(this);
         escribirsd.setOnClickListener(this);
@@ -48,107 +61,41 @@ public class MainActivity extends Activity implements OnClickListener {
         // Código que me comprueba si existe SD y si puedo escribir o no
         String estado = Environment.getExternalStorageState();
 
-        if (estado.equals(Environment.MEDIA_MOUNTED)) {
-            sdDisponible = true;
-            sdAccesoEscritura = true;
-        } else if (estado.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
-            sdDisponible = true;
-            sdAccesoEscritura = false;
-        } else {
-            sdDisponible = false;
-            sdAccesoEscritura = false;
-        }
-
     }//fin onCreate()
 
     @Override
     public void onClick(View v) {
 
+        String contenidoFichero = "";
+
         switch (v.getId()) {
             //----->ESTOS SON LOS CASOS QUE NOS INTERESAN <------
             case (R.id.leersd): //leer SD
-                if (sdDisponible) {
+                
+                textView.setText("");
+        {
+            try {
+                contenidoFichero = manejaFicheroBinario.leerFichero();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 
-                    FileInputStream fis = null;
-                    DataInputStream entrada = null;
-                    String contenido_archivo = ""; //almacenará contenido total del archivo
-
-                    try {
-                        //Apuntamos a dirección de la SD
-                        File ruta_sd = Environment.getExternalStorageDirectory();
-                        File f = new File(ruta_sd.getAbsolutePath(), "ficherosd1.txt");
-
-                        fis = new FileInputStream(f);
-                        entrada = new DataInputStream(fis);
-                        int n = 0; //irá leyendo int por int el archivo
-
-                        //LEEMOS ARCHIVO
-                        while (true) {
-                            n = entrada.readInt();  //se lee  un entero del fichero
-                            contenido_archivo += n;
-                            //System.out.println(n);  //se muestra en pantalla
-                        }
-                    } catch (FileNotFoundException e) {
-                        System.out.println(e.getMessage());
-                    } catch (EOFException e) {
-                        System.out.println("Fin de fichero");
-                    } catch (IOException e) {
-                        System.out.println(e.getMessage());
-                    } finally {
-                        try {
-                            if (fis != null) {
-                                fis.close();
-                            }
-                            if (entrada != null) {
-                                entrada.close();
-                            }
-                        } catch (IOException e) {
-                            System.out.println(e.getMessage());
-                        }
-                    }
-
-                    textView.setText(contenido_archivo);
-
+                if (contenidoFichero.isEmpty() || contenidoFichero.equalsIgnoreCase("")) {
+                    contenidoFichero = "Fichero vacío, primero escribe en él. ";
                 }
+
+                textView.setText(contenidoFichero);
+                Toast.makeText(this, "Se ha leído el fichero", Toast.LENGTH_SHORT).show();
 
                 break;
+
             case (R.id.escribirsd): //Escribir en SD
-                if (sdAccesoEscritura && sdDisponible) { //si la SD está disponible y podemos escribir
 
-                    FileOutputStream fos = null;
-                    DataOutputStream salida = null;
-
-                    try {
-                        File ruta_sd = Environment.getExternalStorageDirectory();
-                        File f = new File(ruta_sd.getAbsolutePath(), "ficherosd1.txt");
-
-                        fos = new FileOutputStream(f);
-                        salida = new DataOutputStream(fos);
-
-                        for (int i = 0; i < 10; i++) {
-                            salida.writeInt(i + 1); //se escribe el número entero en el fichero
-                        }
-
-                        salida.close();
-                    } catch (FileNotFoundException e) {
-                        System.out.println(e.getMessage());
-                    } catch (IOException e) {
-                        System.out.println(e.getMessage());
-                    } finally {
-                        try {
-                            if (fos != null) {
-                                fos.close();
-                            }
-                            if (salida != null) {
-                                salida.close();
-                            }
-                        } catch (IOException e) {
-                            System.out.println(e.getMessage());
-                        }
-                    }
-
-                    break;
-                }
+                textView.setText("");
+                manejaFicheroBinario.llenaAleatorio(100); //llenamos con 10 int aleatorios
+                Toast.makeText(this, "Se ha llenado el fichero con 10 enteros ", Toast.LENGTH_SHORT).show();
+                break;
 
         }//fin switch
 
